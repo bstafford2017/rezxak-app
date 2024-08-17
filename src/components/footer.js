fe89a92bcbaf96c5d5cbbdc63e9d5a1f3346987e
bstafford2017 <b.stafford2017@gmail.com>
2024-08-16 22:15:30 -0500

    Update to jpg

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Logo from '../../public/logo.png';
+import Logo from '../../public/logo.jpg';
 
 const Footer = styled.footer`
   color: #eee;
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import LogoWithName from '../../public/logo-with-text.png';
+import LogoWithName from '../../public/logo-with-text.jpg';
 
 const Navigation = styled.nav``;
 
